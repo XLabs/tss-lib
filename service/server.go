852a6280a12e78c8036c204a5befeef3ed69ab22
Jonathan <jonathan.weiss1@mail.huji.ac.il>
2025-12-04 15:39:45 +0200

    adding unit test and removing unused function

--- service/server.go
+++ service/server.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net"
 	"sync"
-
-	common "github.com/xlabs/tss-common"
 
 	"github.com/xlabs/tss-common/service/signer"
 	"github.com/xlabs/tss-lib/v2/tss"
@@ -14,7 +13,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
@@ -43,6 +41,7 @@ func (s *server) SignMessage(stream signer.Signer_SignMessageServer) error {
 		return err
 	}
 	defer s.removeSubscriber()
+	fmt.Println("New SignMessage subscriber connected")
 
 	ch := make(chan *signer.SignResponse, bufferSize) // Buffered channel for sending status updates
 	errChan := make(chan error, 2)                    // Buffer size 2 to avoid blocking
@@ -109,14 +108,6 @@ func (s *server) requestReader(stream signer.Signer_SignMessageServer, errChan c
 		default:
 			s.logger.Warn("Couldn't inform subscriber about signing error, channel full")
 		}
-	}
-}
-
-func wrapsig(sig *common.SignatureData) *signer.SignResponse {
-	return &signer.SignResponse{
-		Response: &signer.SignResponse_Signature{
-			Signature: proto.CloneOf(sig), // ensures a deep copy
-		},
 	}
 }
 
---
+++ service/server_conn_test.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	common "github.com/xlabs/tss-common"
+	"github.com/xlabs/tss-common/service/signer"
+	"github.com/xlabs/tss-lib/v2/tss"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+)
+
+// ensures that an safe connection is made by checking the peer certificate from the gRPC stream
+func TestSecureConn(t *testing.T) {
+	a := require.New(t)
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	serverSecrets, err := tss.NewGuardianStorageFromFile("../tss/internal/testutils/testdata/tss5/guardian0.json")
+	a.NoError(err)
+	invalidSecrets, err := tss.NewGuardianStorageFromFile("../tss/internal/testutils/testdata/tss5/guardian1.json")
+	a.NoError(err)
+
+	pool := x509.NewCertPool()
+	pool.AddCert(serverSecrets.GetCertificate().Leaf) // only accepting the server's cert.
+
+	socket := "localhost:50051"
+	l, err := net.Listen("tcp", socket)
+	a.NoError(err)
+	defer l.Close()
+
+	serverOpts := []grpc.ServerOption{makeCreds(logger, serverSecrets)}
+	grpcServer := grpc.NewServer(serverOpts...)
+
+	// Mock tss.Signer
+	mockSigner := &mockTssSigner{
+		responses: make(chan *signer.SignResponse),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	srvr := &server{
+		UnimplementedSignerServer: signer.UnimplementedSignerServer{},
+		ctx:                       ctx,
+		cancel:                    cancel,
+		logger:                    logger,
+		Signer:                    mockSigner,
+		listener:                  l,
+		Server:                    grpcServer,
+		mtx:                       sync.Mutex{},
+	}
+
+	signer.RegisterSignerServer(srvr.Server, srvr)
+
+	go func() {
+		if err := srvr.Serve(l); err != nil {
+			logger.Error("gRPC server stopped with error", zap.Error(err))
+		}
+	}()
+	defer srvr.Stop()
+
+	validClientCreds := credentials.NewTLS(&tls.Config{
+		MinVersion:   tls.VersionTLS13,
+		Certificates: []tls.Certificate{*serverSecrets.GetCertificate()},
+		RootCAs:      pool,
+		ServerName:   "localhost", // This should match a SAN in the server's certificate if it has one
+	})
+
+	t.Run("ValidClientCert", func(t *testing.T) {
+		a := require.New(t)
+
+		conn, err := grpc.NewClient(socket, grpc.WithTransportCredentials(validClientCreds))
+		a.NoError(err)
+		defer conn.Close()
+
+		client := signer.NewSignerClient(conn)
+		stream, err := client.SignMessage(ctx)
+		a.NoError(err)
+
+		// ensuring connection is valid by sending a sign request
+		a.NoError(stream.Send(&signer.SignRequest{
+			Digest:    []byte{1, 2, 3, 4, 5},
+			Protocol:  common.ProtocolFROSTSign.ToString(),
+			Committee: [][]byte{}, // no committee for this test
+		}))
+
+		mockSigner.responses <- &signer.SignResponse{}
+
+		msg, err := stream.Recv()
+		a.NoError(err)
+		a.NotNil(msg)
+
+		a.NoError(stream.CloseSend())
+	})
+
+	t.Run("TestErrorOnSigRequest", func(t *testing.T) {
+		conn, err := grpc.NewClient(socket, grpc.WithTransportCredentials(validClientCreds))
+		a.NoError(err)
+		defer conn.Close()
+
+		client := signer.NewSignerClient(conn)
+		stream, err := client.SignMessage(ctx)
+		a.NoError(err)
+
+		mockSigner.returnErrOnSignRequest = true
+		defer func() { mockSigner.returnErrOnSignRequest = false }()
+
+		// ensuring connection is valid by sending a sign request
+		a.NoError(stream.Send(&signer.SignRequest{
+			Digest:    []byte{1, 2, 3, 4, 5},
+			Protocol:  common.ProtocolFROSTSign.ToString(),
+			Committee: [][]byte{}, // no committee for this test
+		}))
+
+		msg, err := stream.Recv()
+		a.NoError(err)
+		a.Equal(codes.FailedPrecondition, codes.Code(msg.Response.(*signer.SignResponse_Status).Status.Code))
+
+		a.NoError(stream.CloseSend())
+	})
+	t.Run("InvalidClientCert", func(t *testing.T) {
+		a := require.New(t)
+
+		conn, err := grpc.NewClient(socket,
+			grpc.WithTransportCredentials(
+				credentials.NewTLS(&tls.Config{
+					Certificates: []tls.Certificate{*invalidSecrets.GetCertificate()},
+					RootCAs:      nil,
+					ServerName:   "localhost",
+				}),
+			),
+		)
+		a.NoError(err)
+		defer conn.Close()
+
+		client := signer.NewSignerClient(conn)
+		_, err = client.SignMessage(ctx) // This will actually establish the connection.
+		a.Error(err)
+		fmt.Println(err)
+	})
+
+}
+
+type mockTssSigner struct {
+	tss.Signer
+	responses              chan *signer.SignResponse
+	returnErrOnSignRequest bool
+}
+
+var errMockSignRequest = errors.New("mock sign request error")
+
+func (m *mockTssSigner) BeginAsyncThresholdSigningProtocol(*signer.SignRequest) error {
+	if m.returnErrOnSignRequest {
+		return errMockSignRequest
+	}
+	return nil
+}
+func (m *mockTssSigner) Responses() <-chan *signer.SignResponse { return m.responses }
